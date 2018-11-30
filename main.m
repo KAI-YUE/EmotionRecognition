1a9ceef1aeb64ed0b98426c12c6fa2decece60bd
Eric.yue <41194196+KAI-YUE@users.noreply.github.com>
2018-11-30 08:40:51 +0800

    Create main.m

---
+++ main.m
@@ -0,0 +1,14 @@
+% CNN Model for CVPR2017
+load('D:\Big_Data\Annotations\BTrainTbl.mat');
+load('D:\Big_Data\Annotations\BValTbl.mat');
+
+augmentOptions = struct('flip',1,'rotate',0);
+
+
+bidsTrain = bindImageDatastore([224 224],BTrainTbl,augmentOptions);
+bidsVal = bindImageDatastore([224 224],BValTbl,augmentOptions);
+Options = loadOptions(bidsVal);
+FullNet = InitNet;
+
+
+trainedNet = trainNetwork(bidsTrain,FullNet,Options);
