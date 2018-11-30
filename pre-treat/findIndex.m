c01b77e5653150c3929b4038dc8882943d66366a
Eric.yue <41194196+KAI-YUE@users.noreply.github.com>
2018-11-30 08:49:44 +0800

    add pre-treat

---
+++ pre-treat/findIndex.m
@@ -0,0 +1,27 @@
+% Input a file name, this function finds the index of it in the annotation.
+% It can locates the flawed data in the original annotations.
+%
+function [idx,order] = findIndex(annotations,fileName)
+found = 0;
+dirRoot = 'D:\Big_Data\emotic';
+order = regexp(fileName,['_person\d*','.'],'match');
+order = order{1};
+order = regexp(order,['\d*'],'match');
+order = order{1};
+for i =1:length(annotations)
+    fullFileName = fullfile(dirRoot,annotations(i).folder,annotations(i).filename);
+    personString = ['_person',order,'.jpg'];
+    fullFileName = strrep(fullFileName,'.jpg',personString);
+    fullFileName = strrep(fullFileName,'images','bodies');
+    if strcmp(fullFileName,fileName)
+        found =1;
+        break;
+    end
+end
+if ~found
+    idx =[];
+    order = [];
+else
+    idx =i;
+    order = str2num(order);
+end
---
+++ pre-treat/findNan.m
@@ -0,0 +1,3 @@
+Tbl = ValTbl;
+idx = isnan(Tbl{2:2:end,28});
+index = 2*find(idx==1);
