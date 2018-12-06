33ae8d3901e2aabb2307617772e0efd17b8aba09
Eric.yue <41194196+KAI-YUE@users.noreply.github.com>
2018-12-06 12:29:09 +0800

    Update main.m
    add specific details

--- main.m
+++ main.m
@@ -1,16 +1,17 @@
 % CNN Model for CVPR2017
 % Change the DirRoot and Options if necessary.
 
-load('D:\Big_Data\Annotations\BTrainTbl.mat');
-load('D:\Big_Data\Annotations\BValTbl.mat');
+load('train.mat');
+load('val.mat');
+
+BValTbl = splitBodies(val);
+BTrainTbl = splitBodies(train);
 
 augmentOptions = struct('flip',1,'rotate',0);
-
 
 bidsTrain = bindImageDatastore([224 224],BTrainTbl,augmentOptions);
 bidsVal = bindImageDatastore([224 224],BValTbl,augmentOptions);
 Options = loadOptions(bidsVal);
 FullNet = InitNet;
-
 
 trainedNet = trainNetwork(bidsTrain,FullNet,Options);
